4d09123200a241b3c68cf392eeebdedb5b2a0636
Eduardo Cordeiro <eduardo.cordeiro1@outlook.com>
2021-06-17 19:39:38 -0300

    linkando com o backend na heroku

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 // definindo a URL onde está localizado a nossa API
 const api = axios.create({
-  baseURL: 'http://localhost:3333'
+  baseURL: window.location.hostname.includes('localhost')
+    ? 'http://localhost:3333'
+    : 'https://volunteer-psychologist.herokuapp.com'
 });
 
 export default api;
